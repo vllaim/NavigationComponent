95a8a0523c2824c17da8eefb37421c0b0dbecbdc
vllaim <kamaeft@gmail.com>
2021-10-07 15:31:56 +0300

    x

--- app/src/main/java/com/kamaeft/navigationcomponent/SecretFragment.kt
+++ app/src/main/java/com/kamaeft/navigationcomponent/SecretFragment.kt
@@ -1,6 +1,27 @@
 package com.kamaeft.navigationcomponent
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kamaeft.navigationcomponent.databinding.FragmentSecretBinding
+
 
 class SecretFragment : Fragment(R.layout.fragment_secret) {
+
+    private lateinit var binding: FragmentSecretBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSecretBinding.bind(view)
+        binding.closeBoxButton.setOnClickListener {
+            // go back to the specified destination
+            findNavController().popBackStack(R.id.rootFragment, false)
+        }
+        binding.goBackButton.setOnClickListener {
+            // go back to the previous screen
+            findNavController().popBackStack()
+        }
+    }
+
 }
